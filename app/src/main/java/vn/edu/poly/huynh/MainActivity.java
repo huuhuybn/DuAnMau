7c577794dfef20e294b07cdc77c66565f3a5f627
HuyNguyen <huuhuybn@gmail.com>
2019-01-07 12:59:53 +0700

    Initial commit

---
+++ app/src/main/java/vn/edu/poly/huynh/MainActivity.java
@@ -0,0 +1,24 @@
+package vn.edu.poly.huynh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvText;
+    private Button btnOK;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvText = findViewById(R.id.tvText);
+        btnOK = findViewById(R.id.btnOK);
+
+
+        
+
+    }
+}
